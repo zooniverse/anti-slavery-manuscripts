987e2c6ca43d4b8983bad39b00ea7b94813ed847
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2017-05-18 12:39:36 +0100

    Minor updates

--- src/components/Home-Page/index.jsx
+++ src/components/Home-Page/index.jsx
@@ -4,7 +4,8 @@ export default class HomePage extends React.Component {
   render() {
     return (
       <main className="app-content home-page">
-        Liberating the Liberator
+        <b>Welcome to Liberating the Liberator.</b> Now let's drop some of that phat Lorem ipsum beat.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget eleifend risus, id aliquet est. Maecenas tempus luctus lacinia. 
       </main>
     );
   }
