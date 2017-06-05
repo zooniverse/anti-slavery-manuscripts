8267f7cff9d28c7ab2ab803ac158e19f4f2848ac
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2017-06-05 17:36:53 +0100

    Classify page, yo

--- src/containers/ClassifierContainer.jsx
+++ src/containers/ClassifierContainer.jsx
@@ -11,7 +11,22 @@ class ClassifierContainer extends React.Component {
   render() {
     return (
       <main className="app-content classifier-page">
-        Beep boop
+        <section className="subject-viewer">
+          <div className="subject-controls">Controls up here</div>
+          <div className="subject-images">
+            <div className="subject-image left">
+            </div>
+            <div className="subject-image right">
+            </div>
+          </div>
+        </section>
+        <section className="classifier-controls">
+          <button href="#" className="white-grey button with-shadow">Tutorial</button>
+          <div className="talk-section">Talk</div>
+          <div className="crib-sheet">Cribsheet</div>
+          <button href="#" className="white-green button with-shadow">Save &amp; Close</button>
+          <button href="#" className="green button with-shadow">Done</button>
+        </section>
       </main>
     );
   }
