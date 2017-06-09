3a338873d4c613f05e169185bfc06c5b1db320a5
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2017-06-09 13:33:59 +0100

    Styles for mini-control lists

--- src/containers/ClassifierContainer.jsx
+++ src/containers/ClassifierContainer.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 //import { checkLoginUser, loginToPanoptes, logoutFromPanoptes } from '../ducks/login';
 
 import TmpSvgIcon from '../components/TmpSvgIcon';
+import TmpImg from '../images/zooniverse-icon-web-white-small.png';
 
 class ClassifierContainer extends React.Component {
   constructor(props) {
@@ -41,8 +42,42 @@ class ClassifierContainer extends React.Component {
         </section>
         <section className="classifier-controls">
           <button href="#" className="white-grey button with-shadow">Tutorial</button>
-          <div className="talk-section">Talk</div>
-          <div className="crib-sheet">Cribsheet</div>
+          <div className="talk-section mini-control">
+            <div className="title">
+              <b>Talk</b>
+              <button className="icon button"><TmpSvgIcon width="15" height="15" /></button>
+            </div>
+            <div className="list">
+              <div className="item">
+                <span className="username">@username</span>
+                <p>Letters from Deborah Weston to Anne Warren Weston</p>
+              </div>
+              <div className="item">
+                <span className="username">@freshprince</span>
+                <p>Now this is a story all about how my life got flipped-turned upside down</p>
+              </div>
+              <button className="icon button"><TmpSvgIcon width="15" height="15" /></button>
+            </div>
+            <button className="dusty-lavender button">View on Talk</button>
+          </div>
+          <div className="crib-sheet mini-control">
+            <div className="title">
+              <b>Crib Sheet</b>
+              <i>Save interesting images for reference</i>
+              <button className="icon button"><TmpSvgIcon width="15" height="15" /></button>
+            </div>
+            <div className="list">
+              <div className="item">
+                <span className="thumbnail"><img src={TmpImg} /></span>
+                <p>Ampersand</p>
+              </div>
+              <div className="item">
+                <span className="thumbnail"><img src={TmpImg} /></span>
+                <p>Zooniverse Logo</p>
+              </div>
+              <button className="icon button"><TmpSvgIcon width="15" height="15" /></button>
+            </div>
+          </div>
           <button href="#" className="white-green button with-shadow">Save &amp; Close</button>
           <button href="#" className="green button with-shadow">Done</button>
         </section>
