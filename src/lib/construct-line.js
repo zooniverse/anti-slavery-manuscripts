488669d2846a04b6c3d3039d44c6611e5c57cc18
Will Granger <twgranger@gmail.com>
2017-10-11 11:20:25 -0500

    Add Existential Checks

--- src/lib/construct-line.js
+++ src/lib/construct-line.js
@@ -2,44 +2,50 @@ import React from 'react';
 
 function constructLines(line) {
   const lines = [];
-  line.clusters_x.map((point, i) => {
-    if (i > 0) {
-      lines.push(
-        <line
-          key={`line${i-1}`}
-          x1={line.clusters_x[i - 1]} y1={line.clusters_y[i - 1]}
-          x2={line.clusters_x[i]} y2={line.clusters_y[i]}
-          stroke="#c33" strokeWidth="2"
-        />
-      );
-    }
-  });
+  if (line && line.clusters_x) {
+    line.clusters_x.map((point, i) => {
+      if (i > 0) {
+        lines.push(
+          <line
+            key={`line${i-1}`}
+            x1={line.clusters_x[i - 1]} y1={line.clusters_y[i - 1]}
+            x2={line.clusters_x[i]} y2={line.clusters_y[i]}
+            stroke="#c33" strokeWidth="2"
+          />
+        );
+      }
+    });
+  }
   return lines;
 }
 
 function constructPoints(line) {
   const points = [];
-  line.clusters_x.map((point, i) => {
-    const x = line.clusters_x[i];
-    const y = line.clusters_y[i];
-    points.push(
-      <circle
-        key={`point${i}`}
-        cx={x} cy={y} r={5} fill="#c33"
-      />
-    );
-  });
+  if (line && line.clusters_x && line.clusters_y) {
+    line.clusters_x.map((point, i) => {
+      const x = line.clusters_x[i];
+      const y = line.clusters_y[i];
+      points.push(
+        <circle
+          key={`point${i}`}
+          cx={x} cy={y} r={5} fill="#c33"
+        />
+      );
+    });
+  }
   return points;
 };
 
 function constructText(line) {
   const sentences = [];
-  line.clusters_text.map((value) => {
-    value.map((word, i) => {
-      if (!sentences[i]) { sentences[i] = []; }
-      if (word.length) { sentences[i].push(word); }
-    })
-  });
+  if (line && line.clusters_text) {
+    line.clusters_text.map((value) => {
+      value.map((word, i) => {
+        if (!sentences[i]) { sentences[i] = []; }
+        if (word && word.length) { sentences[i].push(word); }
+      })
+    });
+  }
   return sentences.map(value => value.join(' '));
 }
 
