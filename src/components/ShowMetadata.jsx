661e19e8ec513570cf63d60ee398eaadbb8ba303
Will Granger <twgranger@gmail.com>
2017-11-08 10:52:55 -0600

    Hide Secret Metadata Columns (#154)
    * Hide Column with Pound
    
    * Use Regex to Account for Other Special Characters

--- src/components/ShowMetadata.jsx
+++ src/components/ShowMetadata.jsx
@@ -9,7 +9,8 @@ const ShowMetadata = ({ metadata }) => {
       <img role="presentation" className="divider" src={Divider} />
       <table width="100%">
         {Object.keys(metadata).map((key, i) => {
-          if (metadata[key]) {
+          const isHidden = /^\s*#|(\/\/)|!/g.test(key);
+          if (metadata[key] && !isHidden) {
             return (
               <tr key={i}>
                 <td><b>{key}</b></td>
