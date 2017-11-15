114937de7f521743208b917333da37e006d7f6f9
Will Granger <twgranger@gmail.com>
2017-11-15 09:50:56 -0600

    Add to Cribsheet

--- src/components/CribSheet.jsx
+++ src/components/CribSheet.jsx
@@ -6,12 +6,33 @@ class CribSheet extends React.Component {
   constructor(props) {
     super(props);
 
+    this.renderItem = this.renderItem.bind(this);
+  }
+
+  renderItem() {
+    return (
+      <div className="crib-sheet__item">
+        <div className="crib-sheet__label">
+          Item Name
+        </div>
+      </div>
+    )
   }
 
   render() {
     return (
-      <div className="collections-manager">
-        <span>Hello World</span>
+      <div className="crib-sheet">
+        <h3>Crib Sheet</h3>
+        <span>Save images and notes for reference.</span>
+        {this.renderItem()}
+        <div className="crib-sheet__item">
+          <button>
+            <i className="fa fa-plus" />
+          </button>
+          <div className="crib-sheet__label">
+            Add New Item
+          </div>
+        </div>
       </div>
     );
   }
