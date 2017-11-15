7165216efe5ac53811a18e063f90089a8213cc9c
Will Granger <twgranger@gmail.com>
2017-11-15 15:23:16 -0600

    Create Announcement Banner for Beta (#170)

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <div className="top-banner">
+      <h2>
+        Thanks for participating in our beta test.
+        Please click on the "Sign in" button at top right
+        to login or create an account.
+      </h2>
+    </div>
+  );
+};
+
+export default Banner;
