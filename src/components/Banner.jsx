506fc531248db49b1bebaf996b2d8c6243dd646f
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2018-01-08 19:01:51 +0000

    Remove banner (#208)

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,14 +2,15 @@
 import React from 'react';
 
 const Banner = () => {
-  return (
+  return null;
+  /*return (
     <div className="beta-top-banner">
       <h2>
         We’re currently implementing some changes to the project based on
         feedback from our volunteers. Stay tuned for our next round of beta testing!
       </h2>
     </div>
-  );
+  );*/
 };
 
 export default Banner;
