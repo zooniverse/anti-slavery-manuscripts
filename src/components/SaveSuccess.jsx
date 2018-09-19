41d3b495413ab64cb4f9488431781011e2a69e80
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-09-19 17:22:55 +0100

    Update SAVE SUCCESS message

--- src/components/SaveSuccess.jsx
+++ src/components/SaveSuccess.jsx
@@ -6,8 +6,14 @@ const SaveSuccess = ({ onClose }) => {
     <div className="save-confirm">
       <h3>Save Success!</h3>
       <span>
-         Your work-in-progress has been saved. As long as you&apos;re logged in, you
-         can visit this page again at a later time to continue where you left off.
+        Your work-in-progress has been saved. As long as you&apos;re logged in, you
+        can visit this page again at a later time to continue where you left off.
+      </span>
+      <span>
+        Please be aware that other volunteers may be working on the same
+        subject in parallel. If the subject is retired by others before you have
+        finished your submission, you will not be able to return to your
+        locally-saved work.
       </span>
       <button className="confirm-button" type="submit" onClick={onClose}>OK</button>
     </div>
