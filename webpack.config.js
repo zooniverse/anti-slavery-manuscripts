82d35794fbd1212704230c9db426302b8a736710
Will Granger <twgranger@gmail.com>
2019-04-10 11:05:03 -0500

    Add markdown anchor exclusion to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
   module: {
     rules: [{
       test: /\.jsx?$/,
-      exclude: /node_modules/,
+      exclude: /node_modules\/(?!(markdown-it-anchor|markdown-it-table-of-contents|striptags)\/).*/,
       use: 'babel-loader',
     }, {
       test: /\.(jpg|png|gif|otf|eot|svg|ttf|woff\d?)$/,
