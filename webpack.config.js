147397a8999e6b749c096388e3d7bf7aa0ffe02d
Will Granger <twgranger@gmail.com>
2019-04-11 05:28:23 -0500

    Update Webpack (#287)
    * Update Polyfill and Add Promise
    
    * Run npm audit
    
    * Update to Webpack 4
    
    * Update Webpack and Babel
    
    * Resize Images and Use Correct Hot Loader
    
    * Working Changes to webpack.config
    
    * Use babel.config.js
    
    * Update Production Webpack
    
    * Update and Install Packages
    
    * Run new audit
    
    * Add markdown anchor exclusion to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,80 +2,85 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const DashboardPlugin = require('webpack-dashboard/plugin');
 const nib = require('nib');
 
 module.exports = {
-
-  devtool: 'eval-source-map',
-
+  mode: 'development',
+  devServer: {
+    allowedHosts: [
+      '.zooniverse.org',
+    ],
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, '/src/'),
+    open: true,
+    inline: true,
+    port: 3000, // Change this for your project
+  },
+  devtool: 'cheap-module-source-map',
   entry: [
     'eventsource-polyfill', // necessary for hot reloading with IE
-    'webpack-hot-middleware/client?reload=true',
     path.join(__dirname, 'src/index.jsx'),
   ],
-
   output: {
     path: path.join(__dirname, '/dist/'),
     filename: '[name].js',
     publicPath: '/',
   },
-
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
     new HtmlWebpackPlugin({
       template: 'src/index.tpl.html',
       inject: 'body',
       filename: 'index.html',
       gtm: '',
     }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('staging'),
-    }),
+    new DashboardPlugin({ port: 3001 }),
   ],
 
   resolve: {
-    extensions: ['.js', '.jsx', '.styl'],
+    extensions: ['*', '.js', '.jsx', '.styl', '.css'],
     modules: ['.', 'node_modules'],
   },
 
   module: {
-    // preLoaders: [
-    //   {
-    //     test: /\.jsx?$/,
-    //     exclude: /node_modules/,
-    //     loader: 'eslint-loader',
-    //   },
-    // ],
     rules: [{
       test: /\.jsx?$/,
-      exclude: /(node_modules)/,
+      exclude: /node_modules\/(?!(markdown-it-anchor|markdown-it-table-of-contents|striptags)\/).*/,
       use: 'babel-loader',
     }, {
-      test: /\.(jpg|png|gif|otf|eot|svg)$/,
+      test: /\.(jpg|png|gif|otf|eot|svg|ttf|woff\d?)$/,
       use: 'file-loader',
     }, {
-      test: /\.(ico|ttf|woff\d?)$/,
-      use: 'file-loader?name=[name].[ext]',
+      test: /\.css$/,
+      use: [{
+        loader: 'style-loader',
+      }, {
+        loader: 'css-loader',
+      }],
     }, {
       test: /\.styl$/,
       use: [{
         loader: 'style-loader',
       }, {
         loader: 'css-loader',
-        options: {
-           includePaths: [path.resolve(__dirname, 'node_modules/zoo-grommet/dist'), path.resolve(__dirname, 'node_modules/zooniverse-react-components/lib/zooniverse-react-components.css')]
-        }
       }, {
         loader: 'stylus-loader',
         options: {
           use: [nib()],
         },
       }],
+    }, {
+      test: /\.(txt|ico)$/,
+      use: [{
+        loader: 'file-loader?name=[name].[ext]',
+      }],
     }],
   },
   node: {
-    fs: "empty"
-  }
+    fs: 'empty',
+  },
 
 };
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,93 +2,96 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const StatsPlugin = require('stats-webpack-plugin');
+const MiniSccExtractPlugin = require('mini-css-extract-plugin');
 const nib = require('nib');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-
   entry: [
     path.join(__dirname, 'src/index.jsx'),
   ],
-
-  output: {
-    path: path.join(__dirname, '/dist/'),
-    filename: '[name]-[hash].min.js',
+  mode: 'production',
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'main',
+          test: /\.(css|styl)$/,
+          chunks: 'all',
+          enforce: true
+        },
+        vendor: {
+          name: 'vendor',
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
   },
-
+  output: {
+    publicPath: '/',
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name]-[chunkhash].min.js',
+    chunkFilename: '[name]-chunkhash.js',
+  },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
     new HtmlWebpackPlugin({
       template: 'src/index.tpl.html',
       inject: 'body',
       filename: 'index.html',
       gtm: '<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WDW6V4" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src="//www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);})(window,document,"script","dataLayer","GTM-WDW6V4");</script>',
     }),
-    new ExtractTextPlugin({
-      filename: '[name]-[hash].min.css',
-      allChunks: true,
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        screw_ie8: true,
-      },
-    }),
-    new StatsPlugin('webpack.stats.json', {
-      source: false,
-      modules: false,
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    new MiniSccExtractPlugin({
+      filename: '[name]-[contenthash].css'
     }),
   ],
-
   resolve: {
-    extensions: ['.js', '.jsx', '.styl', '.css'],
+    extensions: ['*', '.js', '.jsx', '.styl', '.css'],
     modules: ['.', 'node_modules'],
   },
 
   module: {
     rules: [{
       test: /\.jsx?$/,
-      exclude: /node_modules\/(?!(markdown-it-anchor|markdown-it-table-of-contents)\/).*/,
-      // markdown-it-anchor and markdown-it-table-of-contents are written in ES6 and aren't properly compiled
+      exclude: /node_modules\/(?!(markdown-it-anchor|markdown-it-table-of-contents|striptags)\/).*/,
       use: 'babel-loader',
     }, {
       test: /\.css$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: {
-          loader: 'css-loader',
-        },
-      }),
+      use: [
+        MiniSccExtractPlugin.loader,
+        'css-loader',
+      ],
     }, {
       test: /\.styl$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [{
-          loader: 'css-loader',
-          options: {
-             includePaths: [path.resolve(__dirname, 'node_modules/zoo-grommet/dist'), path.resolve(__dirname, 'node_modules/zooniverse-react-components/lib/zooniverse-react-components.css')]
-          }
-        }, {
-          loader: 'stylus-loader',
-          options: {
-            use: [nib()],
-          },
-        }],
-      }),
+      use: [{
+        loader: MiniSccExtractPlugin.loader,
+      }, {
+        loader: 'css-loader',
+      }, {
+        loader: 'stylus-loader',
+        options: {
+          use: [nib()],
+        },
+      }],
     }, {
-      test: /\.(jpg|png|gif|otf|eot|svg)$/,
-      loader: 'file-loader',
+      test: /\.(jpg|png|gif|otf|eot|svg|ttf|woff\d?)$/,
+      use: [{
+        loader: 'file-loader',
+      }, {
+        loader: 'image-webpack-loader',
+      }],
     }, {
-      test: /\.(ico|ttf|woff\d?)$/,
-      loader: 'file-loader?name=[name].[ext]',
+      test: /\.(txt|ico)$/,
+      use: [{
+        loader: 'file-loader?name=[name].[ext]',
+      }],
     }],
   },
   node: {
-    fs: "empty"
-  }
+    fs: 'empty',
+  },
 
 };
